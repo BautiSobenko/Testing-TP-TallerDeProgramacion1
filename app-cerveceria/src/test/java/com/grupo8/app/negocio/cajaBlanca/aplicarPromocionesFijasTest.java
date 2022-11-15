9c639a2e4ac313659a38429bcc9128d8b9a80863
Bautista Priano <bautipriano@gmail.com>
2022-11-15 04:11:33 -0300

    Testing Cobertura caja blanca

---
+++ app-cerveceria/src/test/java/com/grupo8/app/negocio/cajaBlanca/aplicarPromocionesFijasTest.java
@@ -0,0 +1,151 @@
+package com.grupo8.app.negocio.cajaBlanca;
+
+import com.grupo8.app.dto.ProductoDTO;
+import com.grupo8.app.modelo.*;
+import com.grupo8.app.negocio.GestionDeMesas;
+import com.grupo8.app.negocio.cajaBlanca.escenarios.Escenario1;
+import com.grupo8.app.negocio.cajaBlanca.escenarios.Escenario2;
+import com.grupo8.app.negocio.cajaBlanca.escenarios.Escenario3;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class aplicarPromocionesFijasTest {
+
+    Escenario1 escenarioSinPromo;
+    Escenario2 escenarioPromo2x1;
+    Escenario3 escenarioPromoCant;
+    GestionDeMesas gestionDeMesas;
+
+
+    @Before
+    public void setUp() throws Exception {
+        escenarioSinPromo = new Escenario1();
+        escenarioPromo2x1 = new Escenario2();
+        escenarioPromoCant = new Escenario3();
+        gestionDeMesas = new GestionDeMesas();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    /*
+    public class Comanda implements Serializable {
+      private String id;
+      private List<Pedido> pedidos;
+      private EstadoComanda estadoPedido;
+      private Date apertura;
+      private Date cierre;
+      private Mesa mesa;
+     */
+
+    @Test
+    public void C1() {
+        this.escenarioPromoCant.aplicaEscenario3();
+
+        Mesa mesa = new Mesa(1 , 2);
+        Comanda comanda = new Comanda(mesa);
+        Pedido pedido1 = new Pedido(new Producto("Pizza", 20, 10, 20), 4);
+        Pedido pedido2 = new Pedido(new Producto("Fideos", 20, 10, 20), 1);
+        List<Pedido> pedidos = new ArrayList<>();
+        pedidos.add(pedido1);
+        pedidos.add(pedido2);
+        comanda.setPedidos(pedidos);
+
+        CierreComanda cierreComanda = new CierreComanda(comanda);
+        assertFalse("No se deberia haber aplicado promocion", gestionDeMesas.aplicarPromocionesFijas(cierreComanda));
+
+        this.escenarioPromoCant.borraEscenario();
+
+    }
+
+    @Test
+    public void C2() {
+
+        this.escenarioPromoCant.aplicaEscenario3();
+
+        Mesa mesa = new Mesa(1 , 2);
+        Comanda comanda = new Comanda(mesa);
+        Pedido pedido1 = new Pedido(this.escenarioPromoCant.getProducto(), 2);
+        Pedido pedido2 = new Pedido(new Producto("Fideos", 20, 10, 20), 1);
+        List<Pedido> pedidos = new ArrayList<>();
+        pedidos.add(pedido1);
+        pedidos.add(pedido2);
+        comanda.setPedidos(pedidos);
+
+        System.out.println(Empresa.getEmpresa().getPromocionesFijas().getPromocionesFijas());
+
+        CierreComanda cierreComanda = new CierreComanda(comanda);
+        assertFalse("No se deberia haber aplicado promocion", gestionDeMesas.aplicarPromocionesFijas(cierreComanda));
+
+        this.escenarioPromoCant.borraEscenario();
+
+    }
+
+    @Test
+    public void C3() {
+
+        this.escenarioPromo2x1.aplicaEscenario2();
+
+        Mesa mesa = new Mesa(1 , 2);
+        Comanda comanda = new Comanda(mesa);
+        Pedido pedido1 = new Pedido(this.escenarioPromo2x1.getProducto(), 3);
+        Pedido pedido2 = new Pedido(new Producto("Fideos", 20, 10, 20), 1);
+        comanda.getPedidos().add(pedido1);
+        comanda.getPedidos().add(pedido2);
+
+        CierreComanda cierreComanda = new CierreComanda(comanda);
+        assertTrue("Deberia haber aplicado promocion", gestionDeMesas.aplicarPromocionesFijas(cierreComanda));
+
+        this.escenarioPromo2x1.borraEscenario();
+
+    }
+
+    @Test
+    public void C4() {
+
+        this.escenarioPromoCant.aplicaEscenario3();
+
+        Mesa mesa = new Mesa(1 , 2);
+        Comanda comanda = new Comanda(mesa);
+        Producto prodDiferente = new Producto( "Sopa", 20, 10, 5 );
+        Pedido pedido1 = new Pedido( prodDiferente, 3);
+        Pedido pedido2 = new Pedido(new Producto("Fideos", 20, 10, 20), 1);
+        comanda.getPedidos().add(pedido1);
+        comanda.getPedidos().add(pedido2);
+
+        CierreComanda cierreComanda = new CierreComanda(comanda);
+        assertFalse("No deberia haber aplicado promocion", gestionDeMesas.aplicarPromocionesFijas(cierreComanda));
+
+        this.escenarioPromoCant.borraEscenario();
+
+    }
+
+    @Test
+    public void C5() {
+
+        this.escenarioSinPromo.aplicaEscenario1();
+
+        Mesa mesa = new Mesa(1 , 2);
+        Comanda comanda = new Comanda(mesa);
+        Producto prodDiferente = new Producto( "Pizza", 20, 10, 5 );
+        Pedido pedido1 = new Pedido( prodDiferente, 3);
+        Pedido pedido2 = new Pedido(new Producto("Fideos", 20, 10, 20), 1);
+        comanda.getPedidos().add(pedido1);
+        comanda.getPedidos().add(pedido2);
+
+        CierreComanda cierreComanda = new CierreComanda(comanda);
+        assertFalse("No deberia haber aplicado promocion", gestionDeMesas.aplicarPromocionesFijas(cierreComanda));
+
+        this.escenarioSinPromo.borraEscenario();
+
+    }
+}
---
+++ app-cerveceria/src/test/java/com/grupo8/app/negocio/cajaBlanca/escenarios/Escenario1.java
@@ -0,0 +1,42 @@
+package com.grupo8.app.negocio.cajaBlanca.escenarios;
+
+import com.grupo8.app.modelo.Empresa;
+import com.grupo8.app.modelo.Promociones.PromocionFija;
+import com.grupo8.app.wrappers.PromocionesFijasWrapper;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Escenario1 {
+
+    Empresa empresa;
+    PromocionesFijasWrapper promocionesFijas;
+
+    public Escenario1() {
+        empresa = Empresa.getEmpresa();
+    }
+
+    public void aplicaEscenario1(){
+
+        /*
+        empresa = Empresa.getEmpresa();
+
+        promocionesFijas = empresa.getPromocionesFijas();
+        PromocionesFijasWrapper listaPromocionesVacia = new PromocionesFijasWrapper();
+        empresa.setPromocionesFijas(listaPromocionesVacia);
+
+         */
+
+        this.promocionesFijas = empresa.getPromocionesFijas();
+
+        Set<PromocionFija> promocionesFijas = new HashSet<>();
+
+        empresa.getPromocionesFijas().setPromocionesFijas(promocionesFijas);
+
+    }
+
+    public void borraEscenario(){
+        empresa.setPromocionesFijas(promocionesFijas);
+    }
+
+}
